9e3d4fad78af6d85dfbd5bf14dd8d8dae1ec8b82
Daria Ivanova <dashann@mail.ru>
2026-03-19 13:27:31 +0300

    feature: add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
     '@element-plus/nuxt',
-    '@pinia/nuxt'
+    '@pinia/nuxt',
+    ...(process.env.NODE_ENV === 'production' ? ['@vite-pwa/nuxt'] : [])
   ],
 
   devtools: {
@@ -13,7 +14,10 @@ export default defineNuxtConfig({
   app: {
     head: {
       viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0',
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }]
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+        { rel: 'apple-touch-icon', href: '/favicon/apple-touch-icon.png' }
+      ]
     },
     pageTransition: { name: 'page', mode: 'out-in' }
   },
@@ -33,6 +37,55 @@ export default defineNuxtConfig({
           additionalData: '@use "~/assets/css/mixins.scss" as *;'
         }
       }
+    }
+  },
+
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Мой ремонт',
+      short_name: 'Ремонт',
+      description: 'Отслеживайте и управляйте расходами на ремонт',
+      lang: 'ru',
+      theme_color: '#0052a2',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/favicon/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/favicon/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: '/favicon/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,webp,ico,svg,woff,woff2}'],
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'google-fonts-cache',
+            expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 }
+          }
+        }
+      ]
+    },
+    devOptions: {
+      enabled: false
     }
   },
 
